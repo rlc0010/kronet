58f5d2a116e8d6ee0ad71d424655ff4d7c6fe1b7
ndmario <ndiazmario@gmail.com>
2026-03-24 20:12:54 +0100

    RF2: Vista login con formulario

---
+++ app/views/auth/login.php
@@ -0,0 +1,5 @@
+<form method="POST" action="/kronet/public/login">
+    <input type="email" name="email" placeholder="Email" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
