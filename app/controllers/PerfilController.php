850f4a05b1fe21399b9d8fc1933bd47b1770ac30
ndmario <ndiazmario@gmail.com>
2026-05-05 20:20:19 +0200

    feature: ver perfil propio 2.0

--- app/controllers/PerfilController.php
+++ app/controllers/PerfilController.php
@@ -1,10 +1,39 @@
 <?php
+require_once __DIR__ . '/../../config/conexion_db.php';
 require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../models/Intercambio.php';
 
 class PerfilController {
 
     public function verPerfil() {
-        $usuario = User::findById($_SESSION['user']);
-        require __DIR__ . '/../views/perfil/verPerfil.php';
+        session_start();
+
+        // Comprobar login
+        if (!isset($_SESSION['id_usuario'])) {
+            $error = "Debes iniciar sesión";
+            $usuario = [];
+            $historial = [];
+            require __DIR__ . '/../views/verPerfil.php';
+            return;
+        }
+
+        $idUsuario = $_SESSION['id_usuario'];
+
+        // Obtener usuario
+        $usuario = User::findById($idUsuario);
+
+        // Obtener historial
+        $historial = Intercambio::findByUsuario($idUsuario);
+
+        // Error A1
+        if (!$usuario) {
+            $error = "No se han podido cargar los datos";
+        }
+
+        // Evitar errores en la vista
+        $usuario = $usuario ?? [];
+        $historial = $historial ?? [];
+
+        require __DIR__ . '/../views/verPerfil.php';
     }
 }
